47a81d885abcefb61d501e795fa3e0752a875787
Marco Arduini <mar.arduini@gmail.com>
2014-12-15 10:36:21 +0100

    removed useless method from base ApiController

--- app/controllers/Api/api_controller.rb
+++ app/controllers/Api/api_controller.rb
@@ -1,9 +1,5 @@
 module Api
   class ApiController < ApplicationController
     before_action :require_signed_in!
-
-    def require_board_member!
-      redirect_to new_session_url unless current_board.is_member?(current_user)
-    end
   end
 end
